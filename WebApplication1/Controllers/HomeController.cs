dd2342ce30590136a37a09dd5a15580d3cd0c6d3
Yatesu0x00 <61599866+Yatesu0x00@users.noreply.github.com>
2020-12-31 16:26:32 +0100

    Projektdateien hinzufügen.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Encodings.Web;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: /HomeController/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HomeController/test/
+        public string Test() 
+        {
+            return "test action";
+        }
+
+        //Parameter angabe in die URL
+        // GET: /Home/Welcome/{ID}/name=.../alter=...
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult ItWorks(string message = "IT WOOOOORKS! ＼(^o^)／", int loopTimes = 2)
+        {
+            //return HtmlEncoder.Default.Encode($"Name: {name}, Age: {alter}, ID: {ID}");
+            ViewData["Message"] = message;
+            ViewData["LoopTimes"] = loopTimes;
+            
+            if(loopTimes >= 100) 
+            {
+                ViewData["LoopTimes"] = 1;
+                ViewData["Message"] = "Oh you try to loop it over 100 times huh? You are pretty cool! ( •_•)>⌐■-■";
+            }
+
+            return View();
+        }
+    }
+}
